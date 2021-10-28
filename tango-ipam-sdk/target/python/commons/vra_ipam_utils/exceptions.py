a83d5a255b202707067f5eefc1580e1f11805841
radamhenderson <radamhenderson@gmail.com>
2021-10-28 11:54:04 -0500

    removed target

--- tango-ipam-sdk/target/python/commons/vra_ipam_utils/exceptions.py
+++
@@ -1,7 +0,0 @@
-class InvalidCertificateException(Exception):
-    def __init__(self, message, host, port):
-        # Call the base class constructor with the parameters it needs
-        super().__init__(message)
-        
-        self.host = host
-        self.port = port
